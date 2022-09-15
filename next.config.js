78efe8825c76125567891ab0101fa81f80ada156
David Ruisinger <david.ruisinger@gmail.com>
2022-09-15 13:19:37 +0200

    feat: temporarily remove rewrites (#3)

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        destination: '/api/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'api.lfca.ngo',
-          },
-        ],
-        source: '/:path*',
-      },
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       destination: '/api/:path*',
+  //       has: [
+  //         {
+  //           type: 'host',
+  //           value: 'vercel.app',
+  //         },
+  //       ],
+  //       source: '/:path*',
+  //     },
+  //   ]
+  // },
   swcMinify: true,
 }
 
