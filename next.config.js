cd6a2b96bf492bf3b95f7b2876d89c539f17d5e7
David Ruisinger <david.ruisinger@gmail.com>
2022-09-15 15:32:44 +0200

    fix: cors allowed host

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Access-Control-Allow-Origin',
-            value: `https://${process.env.API_HOST}`,
+            value: `https://${process.env.DOCS_HOST}`,
           },
           {
             key: 'Access-Control-Allow-Methods',
