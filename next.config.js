0cc85ed28e9e463b9d0760cf408742028a6f09ad
David Ruisinger <david.ruisinger@gmail.com>
2022-09-15 14:42:38 +0200

    fix: auth rewrite on api.lfca.ngo (#7)

--- next.config.js
+++ next.config.js
@@ -2,20 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
-    return {
-      beforeFiles: [
-        {
-          destination: '/api/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'api.lfca.ngo',
-            },
-          ],
-          source: '/:path*',
-        },
-      ],
-    }
+    return [
+      {
+        destination: '/api/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'api.lfca.ngo',
+          },
+        ],
+        source: '/:path*',
+      },
+    ]
   },
   swcMinify: true,
 }
