907ca18b06e99e97aab1260dfe4f8790f76e9728
David Ruisinger <david.ruisinger@gmail.com>
2022-09-15 12:16:14 +0200

    feat: add rewrite config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        destination: '/api/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'api.lfca.ngo',
+          },
+        ],
+        source: '/:path*',
+      },
+    ]
+  },
   swcMinify: true,
 }
 
