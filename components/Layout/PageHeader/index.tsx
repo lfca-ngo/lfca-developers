58f1980b4b80d027fd87a79dd7b7a15dd5bf78a0
Timo Müller <timo.h.mueller@gmail.com>
2022-09-28 15:46:58 +0300

    finish basic layout

--- components/Layout/PageHeader/index.tsx
+++ components/Layout/PageHeader/index.tsx
@@ -1,5 +1,39 @@
-import styles from './styles.module.less'
+import { Menu } from 'antd'
+import Image from 'next/image'
 
+import styles from './styles.module.less'
 export const PageHeader = () => {
-  return <header className={styles['page-header']}>Header</header>
+  return (
+    <header className={styles['page-header']}>
+      <div className="logo-wrapper">
+        <Image
+          alt="lfca-logo"
+          height={50}
+          layout="fixed"
+          src={'/img/logo.svg'}
+          width={50}
+        />
+        <div className="title">
+          <b className="bold">api</b>
+          docs
+        </div>
+      </div>
+
+      <nav>
+        <Menu
+          items={[
+            {
+              key: '/',
+              label: 'Home',
+            },
+            {
+              key: 'docs',
+              label: 'Documentation',
+            },
+          ]}
+          mode="horizontal"
+        />
+      </nav>
+    </header>
+  )
 }
